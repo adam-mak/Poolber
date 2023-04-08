fc40ce130cd229deeaf9dc048e2e62dfc24364fe
adam-mak <maka9@mcmaster.ca>
2023-04-08 04:27:35 -0400

    draft 2

--- D4/CarpoolProjectExpo/App.js
+++ D4/CarpoolProjectExpo/App.js
@@ -4,11 +4,24 @@ import { StyleSheet, Text, View } from "react-native";
 import HomePage from "./components/HomePage";
 import UserProfile from "./components/UserProfile";
 import Rating from "./components/Rating";
+import RegisterPage from "./components/RegisterPage";
+
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [loaded] = useFonts({
+    Lato: require('./assets/fonts/Lato-Regular.ttf'),
+    UberMoveMedium: require('./assets/fonts/UberMoveMedium.otf'),
+    UberMoveBold: require('./assets/fonts/UberMoveBold.otf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <>
-      <Rating />
+      <RegisterPage />
       {/* <HomePage /> */}
       {/* <View style={styles.container}>
 		<Text>Open up App.js to start working on your app!</Text>
---
+++ D4/CarpoolProjectExpo/components/RegisterPage.js
@@ -0,0 +1,119 @@
+import { Button, Pressable, StyleSheet, Text, View, Image, TextInput } from "react-native";
+
+import { useState } from "react";
+
+const RegisterPage = () => {
+  const [usernameText, setUsernameText] = useState('');
+  const [passwordText, setPasswordText] = useState('');
+
+  return (
+    <View>
+      <View style={styles.mainContainer}>
+        <View style={styles.logoContainer}>
+          <Image source={require("../assets/images/logo.png")}/>
+        </View>
+        <View style={styles.sloganContainer}>
+          <Text style={styles.text}> Carpool for a better tomorrow. </Text>
+        </View>
+        <View style={styles.textInputContainer}>
+          <Image source={require("../assets/images/username_icon.png")}/>
+          <TextInput
+            style={styles.usernameField}
+            value={usernameText}
+            onChangeText={setUsernameText}
+            placeholder="Username"
+          />
+        </View>
+        <View style={styles.textInputContainer}>
+          <Image source={require("../assets/images/password_icon.png")}/>
+          <TextInput
+            style={styles.passwordField}
+            value={passwordText}
+            onChangeText={setPasswordText}
+            placeholder="Password"
+          />
+        </View>
+        <View style={styles.signInContainer}>
+          <Text style={styles.text}> Sign in </Text>
+          <Pressable>
+            <Image source={require("../assets/images/advance_button.png")}/>
+          </Pressable>
+        </View>
+        <View style={styles.registerContainer}>
+          <Text style={styles.text}> Register </Text>
+          <Pressable>
+            <Image source={require("../assets/images/advance_button.png")}/>
+          </Pressable>
+        </View>
+      </View>
+      <View style={styles.bgCurve1Container}>
+        <Image source={require("../assets/images/bg_curve_3.png")}/>
+      </View>
+      <View style={styles.bgCurve2Container}>
+        <Image source={require("../assets/images/bg_curve_4.png")}/>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    marginTop: 153,
+    marginHorizontal: 45,
+  },
+  logoContainer: {
+    alignItems: "center"
+  },
+  sloganContainer: {
+    alignItems: "center",
+    marginTop: 22,
+  },
+  textInputContainer: {
+    flexDirection: "row",
+    marginTop: 45,
+    borderRadius: 40,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    alignItems: "center",
+    backgroundColor: "white",
+    elevation: 5,
+  },
+  signInContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    marginTop: 85,
+    gap: 15,
+  },
+  registerContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    marginTop: 31,
+    gap: 15,
+  },
+  usernameField: {
+    flex: 1,
+    marginLeft: 10,
+    fontFamily: "Lato",
+    fontSize: 15,
+  },
+  passwordField: {
+    flex: 1,
+    marginLeft: 10,
+    fontFamily: "Lato",
+    fontSize: 15,
+  },
+  text: {
+    fontFamily: "UberMoveBold",
+    fontSize: 24,
+    textAlign: "center",
+  },
+  bgCurve1Container: {
+    position: "absolute",
+  },
+  bgCurve2Container: {
+    position: "absolute",
+    bottom: -95,
+  },
+});
+
+export default RegisterPage;
