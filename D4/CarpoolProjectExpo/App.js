a8a750d093c7a83cbc3657f649e80fc25e092026
Jonny <jonathanrsjiang@gmail.com>
2023-04-09 23:25:26 -0400

    arrive page

--- D4/CarpoolProjectExpo/App.js
+++ D4/CarpoolProjectExpo/App.js
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { useFonts } from "expo-font";
 import { FIREBASE_API_KEY } from "@env";
+import ArriveAtDestinationPage from "./pages/ArriveDestinationPage";
 
 import Navigator from "./routes/stack";
 
